7cddd4758e8bf64987082f00920ef1c81d441340
pthao-nb <p.thao.221205@gmail.com>
2025-04-27 10:04:14 +0700

    Add files via upload

---
+++ lamnhom/tracuuphatnguoi.php
@@ -0,0 +1,52 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tra cứu phương tiện</title>
+    <link rel="stylesheet" href="tracuuphatnguoi.css">
+</head>
+<body>
+        <div class="container-phatnguoi">
+        <div class="search-box">
+            <div class="title">TRA CỨU PHƯƠNG TIỆN VI PHẠM GIAO THÔNG QUA HÌNH ẢNH</div>
+            <div class="form-group">
+                <label for="Bienkiemsoat">Biển kiểm soát</label>
+                <input type="text" name="Bienkiemsoat" placeholder="VD: 77H178945">
+            </div>
+            <div class="form-group">
+                <label for="Loaiphuongtien">Loại phương tiện</label>
+                <select name="Loaiphuongtien">
+                    <option value="oto">Ô tô</option>
+                    <option value="xemay">Xe máy</option>
+                    <option value="xemaydien">Xe máy điện</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="Mabaomat">Mã bảo mật</label>
+                <input type="text" name="Mabaomat">
+            </div>
+            <button class="search-button">Tra cứu</button>
+        </div>
+
+        <div class="search-box">
+            <div class="title">TRA CỨU XE MẤT ĐĂNG KÝ, BIỂN SỐ, HẾT NIÊN HẠN, MẤT CẤP</div>
+            <div class="form-group">
+                <label for="bienkiemsoat2">Biển kiểm soát</label>
+                <input type="text" id="Bienkiemsoat2" placeholder="VD: 77H178945">
+            </div>
+            <div class="form-group">
+                <label for="Loaiphuongtien2">Loại phương tiện</label>
+                <select id="Loaiphuongtien2">
+                    <option value="oto">Ô tô</option>
+                    <option value="xemay">Xe máy</option>
+                    <option value="xemaydien">Xe máy điện</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="Mabaomat">Mã bảo mật</label>
+                <input type="text" name="Mabaomat">
+            </div>
+            <button class="search-button">Tra cứu</button>
+        </div>
+    </div>
+    </div>
+</body>
